ed0168b97b47c2afe9d913e7144187ada045e5c2
Mark-a-Hamilton <154675184+Mark-a-Hamilton@users.noreply.github.com>
2024-11-26 19:12:50 +0000

    2024/11/26-19:12

---
+++ MinAPI/EndPoints/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    // public DbSet<ModelName> TableName { get; set; }
+    #region Leaf / Data Tables - No References
+    #endregion
+
+    #region Branch / Transaction Tables - With References
+    #endregion
+}
